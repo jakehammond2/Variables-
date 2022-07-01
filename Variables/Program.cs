70c2a7ccb4791fd3e29fdce1c79dd0eb66aa6fcc
Jake Hammond <jakehammond226@gmail.com>
2022-07-01 11:36:58 -0500

    Variables

---
+++ Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string myName = "Jake";
+int age = 23;
+bool lovesSummer = true; 
+decimal myWeight = 187.5m;
+double runDistance = 20;
+char letter = '!';           
+
+
+Console.WriteLine($"My name is {myName}. I am {age} years old. I weight {myWeight} pounds. I run {runDistance} miles per week{letter} I love summer, it's {lovesSummer}. ");
+
